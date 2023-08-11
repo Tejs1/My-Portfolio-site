9355752fb273d2b30540b0d652d683d3dbb23660
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2023-08-11 15:46:53 +0530

    add comments

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const ham = document.querySelector('.ham');
 const sidebar = document.querySelector('.sidebar');
+
+// open and close sidebar when clicking hamburger
 ham.addEventListener('click', function(event) {
     event.stopPropagation();
     sidebar.classList.toggle('active');
 });
+
+// close sidebar when clicking outside
 document.addEventListener('click', function(event) {
     if (!sidebar.contains(event.target)) {
       sidebar.classList.remove('active');
